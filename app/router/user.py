987a9f5d7a4e82a674048e4e7accbbca00886a95
SinHT0108 <somu950108@gmail.com>
2023-04-27 20:23:01 +0900

    feat: user API endpoint add tags

--- app/router/user.py
+++ app/router/user.py
@@ -17,35 +17,39 @@ router = APIRouter(
 )
 
 
-@router.post(
-    "",
-    description="회원가입",
-    response_model=CreateUserResponse,
-)
+@router.post("", description="회원가입", response_model=CreateUserResponse, tags=["user"])
 async def register(request: CreateUserRequest):
     pass
 
 
-@router.get("", description="회원정보 조회", response_model=GetUserInfoResponse)
+@router.get(
+    "", description="회원정보 조회", response_model=GetUserInfoResponse, tags=["user"]
+)
 async def get_user_info(user_id: CurrentUserId):
     pass
 
 
-@router.patch("", description="회원정보 수정", response_model=EditUserInfoResponse)
+@router.patch(
+    "", description="회원정보 수정", response_model=EditUserInfoResponse, tags=["user"]
+)
 async def edit_user_info(request: EditUserInfoRequest, user_id: CurrentUserId):
     pass
 
 
-@router.patch("/change_password", description="비밀번호 변경", response_model=None)
+@router.patch(
+    "/change_password", description="비밀번호 변경", response_model=None, tags=["user"]
+)
 async def change_password(request: ChangePasswordRequest, user_id: CurrentUserId):
     pass
 
 
-@router.post("/check_password", description="현재 비밀번호 확인", response_model=None)
+@router.post(
+    "/check_password", description="현재 비밀번호 확인", response_model=None, tags=["user"]
+)
 async def check_password(user_id: CurrentUserId):
     pass
 
 
-@router.delete("", description="회원 탈퇴", response_model=None)
+@router.delete("", description="회원 탈퇴", response_model=None, tags=["user"])
 async def delete_user(user_id: CurrentUserId):
     pass
