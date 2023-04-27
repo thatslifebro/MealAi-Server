de4a2fdc223de43079fd67aef565b8b1f8ff44ae
SinHT0108 <somu950108@gmail.com>
2023-04-27 16:13:38 +0900

    feat: user API endpoint

--- app/router/user.py
+++ app/router/user.py
@@ -1,3 +1,15 @@
+from app.dto.user.UserRequest import (
+    CreateUserRequest,
+    EditUserInfoRequest,
+    CurrentUserId,
+    ChangePasswordRequest,
+    CheckPasswordRequest,
+)
+from app.dto.user.UserResponse import (
+    CreateUserResponse,
+    GetUserInfoResponse,
+    EditUserInfoResponse,
+)
 from fastapi import APIRouter
 
 router = APIRouter(
@@ -5,21 +17,35 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-def f1():
-    a1 = 1
-    a2 = 2
-    a3 = 3
-    res = {"a1": a1, "a2": a2, "a3": a3}
+@router.post(
+    "",
+    description="회원가입",
+    response_model=CreateUserResponse,
+)
+async def register(request: CreateUserRequest):
+    pass
 
-    return res
+
+@router.get("", description="회원정보 조회", response_model=GetUserInfoResponse)
+async def get_user_info(user_id: CurrentUserId):
+    pass
 
 
-@router.post("/")
-def f2(c: int):
-    a1 = 1
-    a2 = 2
-    a3 = 3
-    res = {"a1": a1, "a2": a2, "a3": c}
+@router.patch("", description="회원정보 수정", response_model=EditUserInfoResponse)
+async def edit_user_info(request: EditUserInfoRequest, user_id: CurrentUserId):
+    pass
 
-    return res
+
+@router.patch("/change_password", description="비밀번호 변경", response_model=None)
+async def change_password(request: ChangePasswordRequest, user_id: CurrentUserId):
+    pass
+
+
+@router.post("/check_password", description="현재 비밀번호 확인", response_model=None)
+async def check_password(user_id: CurrentUserId):
+    pass
+
+
+@router.delete("", description="회원 탈퇴", response_model=None)
+async def delete_user(user_id: CurrentUserId):
+    pass
