37d1c6f9540da9660740fa8ef9936a2ad9a1679e
[AI6/2반]김성연 <sy9804071@gmail.com>
2023-04-27 17:11:05 +0900

    fix : feed API spec

---
+++ app/dto/feed/FeedRequest.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, Field
+from typing import List
+from enum import Enum
+
+
+class PurposeEnum(str, Enum):
+    balance = "balance"
+    diet = "diet"
+    muscle = "muscle"
+    lchf = "lchf"
+
+
+class MealTimeEnum(str, Enum):
+    breakfast = "breakfast"
+    lunch = "lunch"
+    dinner = "dinner"
+    snack = "snack"
+
+
+class FilterEnum(str, Enum):
+    popularity = "popularity"
+    newest = "newest"
+
+
+class Food(BaseModel):
+    food_id: int = Field(..., title="음식 id")
+    image_url: str = Field(..., title="음식 별 사진 URL")
+    weight: float = Field(..., title="음식의 양")
+
+
+class PostFeed(BaseModel):
+    image_url: str = Field(..., title="전체 이미지 URL")
+    meal_time: MealTimeEnum = Field(..., title="식사 종류", description="B,L,D,S")
+    date: str = Field(..., title="식사 날짜")
+    foods: List[Food] = Field(..., title="먹은 음식")
+    open: int = Field(..., title="공개 여부", description="0 or 1")
+
+
+class PatchFeedData(BaseModel):
+    meal_time: MealTimeEnum = Field(..., title="식사 종류", description="B,L,D,S")
+    date: str = Field(..., title="식사 날짜")
+    foods: List[Food] = Field(..., title="먹은 음식들")
+    open: int = Field(..., title="공개 여부", description="0 or 1")
--- app/router/feed.py
+++ app/router/feed.py
@@ -1,47 +1,79 @@
 from fastapi import APIRouter
 from pydantic import BaseModel, Field
 from typing import List
-from app.dto.feed.RequestFeed import *
-from app.dto.feed.ResponseFeed import *
+from app.dto.feed.FeedRequest import *
+from app.dto.feed.FeedResponse import *
 from enum import Enum
+
 
 router = APIRouter(
     prefix="/api/feeds",
 )
 
 
-@router.post("/")
+@router.post(
+    "",
+    description="피드 작성",
+    response_model=None,
+)
 def post_feed(req: PostFeed) -> str:
-    return "ok"
+    pass
 
 
-@router.get("/")
+@router.get(
+    "",
+    description="전체 피드 조회",
+    response_model=List[FeedData],
+)
 def get_feeds(
-    filter: str, purpose: str, page: int = 1, per_page: int = 7
-) -> List[FeedData]:
-    return
+    purpose: PurposeEnum,
+    filter: FilterEnum = "newest",
+    page: int = 1,
+    per_page: int = 7,
+):
+    pass
 
 
-@router.get("/{feed_id}")
-def get_feed_by_id(feed_id: int) -> FeedData:
-    return
+@router.get(
+    "/{feed_id}",
+    description="상세 피드 조회",
+    response_model=FeedData,
+)
+def get_feed_by_id(feed_id: int):
+    pass
 
 
-@router.patch("/{feed_id}")
+@router.patch(
+    "/{feed_id}",
+    description="피드 수정",
+    response_model=FeedData,
+)
 def patch_feed_by_id(feed_id: int, req: PatchFeedData) -> FeedData:
-    return
+    pass
 
 
-@router.delete("/{feed_id}")
-def delete_feed_by_id(feed_id: int) -> str:
-    return "ok"
+@router.delete(
+    "/{feed_id}",
+    description="피드 삭제",
+    response_model=None,
+)
+def delete_feed_by_id(feed_id: int):
+    pass
 
 
-@router.patch("/likes/{feed_id}")
-def patch_likes_by_id(feed_id: int) -> str:
-    return "ok"
+@router.patch(
+    "/likes/{feed_id}",
+    description="좋아요 토글",
+    response_model=None,
+)
+def patch_likes_by_id(feed_id: int):
+    pass
 
 
-@router.get("/likes")
-def get_my_likes() -> List[FeedData]:
-    return
+@router.get(
+    "/likes",
+    description="내가 좋아요한 피드",
+    response_model=List[FeedData],
+)
+def get_my_likes():
+    pass
