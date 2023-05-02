d7044437a24ed9a5839fb084e6b22a386519c485
[AI6/2반]김성연 <sy9804071@gmail.com>
2023-05-02 18:01:06 +0900

    feat : add get_my_likes dao

--- app/dao/like.py
+++ app/dao/like.py
@@ -22,3 +22,14 @@ def patch_likes(feed_id: int, user_id: int):
             conn.commit()
 
         return "ok"
+
+
+def get_my_likes(user_id: int):
+    with engine.connect() as conn:
+        data = {"user_id": user_id}
+        statement = text(
+            """SELECT * FROM Feed AS F LEFT OUTER JOIN Likes AS L ON F.feed_id = L.feed_id WHERE L.user_id = :user_id"""
+        )
+        result = conn.execute(statement, data)
+        res = result.mappings().all()
+        return res
