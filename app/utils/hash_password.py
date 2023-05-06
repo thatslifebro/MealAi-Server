9832e5edbb7da65cc2aa04d466f2a57d7386052a
[AI6/2반] 신희태 <somu950108@gmail.com>
2023-05-06 06:13:29 +0000

    Revert "Merge branch 'dev/server' into 'master'"
    This reverts merge request !1

--- app/utils/hash_password.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-
-def hash_password(password: str) -> bytes:
-    password_bytes = password.encode("utf-8")
-
-    hashed_bytes = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
-
-    return hashed_bytes
