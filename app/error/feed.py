32554d6e5de6f26f82399d2b65901fe6dd77edd4
SinHT0108 <somu950108@gmail.com>
2023-05-09 21:36:24 +0900

    feat : add error_code

--- app/error/feed.py
+++ app/error/feed.py
@@ -2,24 +2,28 @@ from app.error.base import CustomException
 
 
 class NotFeedOwnerException(CustomException):
-    code = 403
+    status = 403
+    error_code = 3001
     error_name = "NotFeedOwner"
     message = "해당 피드의 작성자가 아닙니다."
 
 
 class NoFeedIdException(CustomException):
-    code = 404
+    status = 404
+    error_code = 3002
     error_name = "NoFeedId"
     message = "해당 아이디의 피드가 없습니다."
 
 
 class DeleteFeedException(CustomException):
-    code = 404
+    status = 404
+    error_code = 3003
     error_name = "DeleteFeedError"
     message = "해당 피드를 삭제할 수 없습니다."
 
 
 class UpdateFeedException(CustomException):
-    code = 404
+    status = 404
+    error_code = 3004
     error_name = "UpdateFeedError"
     message = "해당 피드를 수정할 수 없습니다."
--- app/error/user.py
+++ app/error/user.py
@@ -2,24 +2,28 @@ from app.error.base import CustomException
 
 
 class DuplicatedEmailException(CustomException):
-    code = 400
+    status = 400
+    error_code = 1001
     error_name = "DUPLICATED_EMAIL"
     message = "중복된 Email이 존재합니다."
 
 
 class NotFoundUserException(CustomException):
-    code = 404
+    status = 404
+    error_code = 1002
     error_name = "USER_NOT_FOUND"
-    message = "DB에서 User를 찾을 수 없습니다."
+    message = "User를 찾을 수 없습니다."
 
 
 class NotMatchPasswordException(CustomException):
-    code = 401
+    status = 401
+    error_code = 1003
     error_name = "PASSWORD_NOT_MATCH"
     message = "패스워드가 일치하지 않습니다."
 
 
 class DeletedEmailException(CustomException):
-    code = 400
+    status = 410
+    error_code = 1004
     error_name = "DELETED_USER"
-    message = "이미 탈퇴된 Email 입니다."
+    message = "탈퇴한 Email입니다."
