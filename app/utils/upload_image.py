c50214ef4fcd191441af5a94705452e2336865c3
[AI6/2반]김성연 <sy9804071@gmail.com>
2023-05-10 18:28:06 +0900

    fix : error

---
+++ app/utils/upload_image.py
@@ -0,0 +1,45 @@
+import boto3
+from botocore.exceptions import ClientError
+
+from starlette.config import Config
+from datetime import datetime
+
+config = Config(".env")
+
+S3_ACCESS_KEY = config("S3_ACCESS_KEY")
+S3_SECRET_KEY = config("S3_SECRET_KEY")
+
+client_s3 = boto3.client(
+    "s3", aws_access_key_id=S3_ACCESS_KEY, aws_secret_access_key=S3_SECRET_KEY
+)
+
+
+def upload_file(file, user_id):
+    try:
+        name = (
+            str(user_id)
+            + "/"
+            + str(datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S"))
+            + "-"
+            + file.filename
+        )
+        client_s3.upload_fileobj(
+            file.file,
+            "elice-8team-s3",
+            name,
+        )
+        return name
+    except ClientError as e:
+        print(f"Credential error => {e}")
+    except Exception as e:
+        print(f"Another error => {e}")
+
+
+def get_file(name):
+    try:
+        with open("a.jpg", "wb") as f:
+            client_s3.download_fileobj("elice-8team-s3", name, f)
+    except ClientError as e:
+        print(f"Credential error => {e}")
+    except Exception as e:
+        print(f"Another error => {e}")
